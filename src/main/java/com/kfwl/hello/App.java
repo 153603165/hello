257e3d08f8ce78f75d1ed25f4cd2dc79b89b1b49
Ex＇7 <153603165@qq.com>
2018-07-24 14:37:46 +0800

    初次提交

---
+++ src/main/java/com/kfwl/hello/App.java
@@ -0,0 +1,27 @@
+package com.kfwl.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+@RestController
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	/**
+	 * 测试
+	 * 
+	 * @return
+	 */
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello World";
+	}
+}
